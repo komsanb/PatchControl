e81eacd3e92e291c76cce75e382764af06829f73
Satikan Noothong <s5530213014@phuket.psu.ac.th>
2015-08-24 14:19:03 +0700

    Update folder view and angularJS.

--- Patch_Control/js/angularJS/app.js
+++ Patch_Control/js/angularJS/app.js
@@ -1,1 +1,17 @@
-﻿
+﻿var app = angular.module('myApp', ['ngRoute']);
+
+app.config(['$routeProvider',
+       function ($routeProvider) {
+           $routeProvider.
+              when('/test', {
+                  templateUrl: 'views/test.html',
+                //  controller: 'AddStudentController'
+              }).
+              when('/viewStudents', {
+                  templateUrl: 'viewStudents.htm',
+                  controller: 'ViewStudentsController'
+              }).
+              otherwise({
+                  redirectTo: '/index.html'
+              });
+       }]);

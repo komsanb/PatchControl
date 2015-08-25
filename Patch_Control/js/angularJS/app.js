a14c6aa20bc6c9d9473b7253f3c9a82b3de13bb4
Satikan Noothong <s5530213014@phuket.psu.ac.th>
2015-08-25 15:58:42 +0700

    permission admin.

--- Patch_Control/js/angularJS/app.js
+++ Patch_Control/js/angularJS/app.js
@@ -3,14 +3,26 @@
 app.config(['$routeProvider',
        function ($routeProvider) {
            $routeProvider.
-              when('/test', {
-                  templateUrl: 'views/test.html',
-                //  controller: 'AddStudentController'
+              //when('/test', {
+              //    templateUrl: 'views/test.html',
+              //  //  controller: 'AddStudentController'
+              //}).
+              when('/permission_staff', {
+                  templateUrl: 'views/permission_staff.html',
+                 // controller: 'ViewStudentsController'
               }).
-              when('/viewStudents', {
-                  templateUrl: 'viewStudents.htm',
-                  controller: 'ViewStudentsController'
-              }).
+               when('/staff', {
+                   templateUrl: 'views/staff.html',
+                   // controller: 'ViewStudentsController'
+               }).
+               when('/add_permission', {
+                   templateUrl: 'views/add_permission.html',
+                   // controller: 'ViewStudentsController'
+               }).
+               when('/add_staff', {
+                   templateUrl: 'views/add_staff.html',
+                   // controller: 'ViewStudentsController'
+               }).
               otherwise({
                   redirectTo: '/index.html'
               });
